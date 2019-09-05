82e7f6306e28d6c0d8e54230313e159b6a73fb27
Ly Nguyen <Ly_N_Nguyen@navyfederal.org>
2019-09-05 15:50:13 -0400

    rename projects to have boilerplate as 0

---
+++ 2-custom-geometry/script.js
@@ -0,0 +1,43 @@
+let scene, camera, renderer, cube;
+let ADD = 0.01;
+
+let createCube = function() {
+	let geometry = new THREE.BoxGeometry(.5,.5,.5);
+	let material = new THREE.MeshBasicMaterial({color: 'coral'});
+	cube = new THREE.Mesh(geometry, material);
+	scene.add(cube);
+}
+
+let init = () => {
+
+	// Create the scene.
+	scene = new THREE.Scene();
+	scene.background = new THREE.Color('#000');
+
+	// Create and locate the camera.
+	camera = new THREE.PerspectiveCamera(50,
+								window.innerWidth / window.innerHeight, 
+								1, 1000);
+	camera.position.z = 5;
+
+	// Create cube.
+	createCube();
+
+	// Create the renderer.
+	renderer = new THREE.WebGLRenderer();
+	renderer.setSize(window.innerWidth, window.innerHeight);
+
+	document.body.appendChild(renderer.domElement);
+
+}
+
+let mainLoop = () => {
+
+	renderer.render(scene, camera);
+	requestAnimationFrame(mainLoop);
+}
+
+init();
+mainLoop();
+
+
