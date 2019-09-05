a73104c02b174f4b47025e898581101800ec1aca
Ly Nguyen <Ly_N_Nguyen@navyfederal.org>
2019-09-05 15:48:18 -0400

    remove project 0 info to have boilerplate

--- 1-custom-geometry/script.js
+++ 1-custom-geometry/script.js
@@ -8,46 +8,6 @@ let ADD = 0.01;
 // 	scene.add(cube);
 // }
 
-let createSphere = () => {
-	let geometry = new THREE.SphereGeometry(.5, 100, 100);
-	
-	let material = new THREE.MeshBasicMaterial({color: 0x945E2B, wireframe: false});
-				 
-	sphere = new THREE.Mesh( geometry, material );
-	scene.add(sphere);
-};
-
-let createRing = () => {
-	let geometry = new THREE.TorusGeometry(.7,.15,2,50);
-	
-	let material = new THREE.MeshBasicMaterial({color: 0xD4D272, wireframe: false});
-	// let material = new THREE.MeshBasicMaterial({color: 'yellow', wireframe: false});
-				 
-	torus = new THREE.Mesh( geometry, material );
-	scene.add(torus);
-};
-
-let createRing2 = () => {
-	// let geometry = new THREE.TorusGeometry(.5,.08,2,100,2*Math.PI);
-	let geometry = new THREE.TorusGeometry(1.1,.15,2, 50);
-	
-	let material = new THREE.MeshBasicMaterial({color: 0xF4D272, wireframe: false});
-	// let material = new THREE.MeshBasicMaterial({color: 'orange', wireframe: false});
-				 
-	torus2 = new THREE.Mesh( geometry, material );
-	scene.add(torus2);
-};
-
-let createRing3 = () => {
-	// let geometry = new THREE.TorusGeometry(.92,.1,2,100,2*Math.PI);
-	let geometry = new THREE.TorusGeometry(1.5,.15,2,50);
-	
-	let material = new THREE.MeshBasicMaterial({color: 0xFBB56D, wireframe: false});
-				 
-	torus3 = new THREE.Mesh( geometry, material );
-	scene.add(torus3);
-};
-
 let init = () => {
 
 	// Create the scene.
@@ -60,11 +20,7 @@ let init = () => {
 								1, 1000);
 	camera.position.z = 5;
 
-	// createCube();
-	createSphere();
-	createRing();
-	createRing2();
-	createRing3();
+
 
 	// Create the renderer.
 	renderer = new THREE.WebGLRenderer();
@@ -75,18 +31,6 @@ let init = () => {
 }
 
 let mainLoop = () => {
-
-	sphere.rotation.x += ADD;
-	sphere.rotation.y += ADD;
-
-	torus.rotation.x += ADD;
-	torus.rotation.y += ADD;
-
-	torus2.rotation.x += ADD;
-	torus2.rotation.y += ADD;
-
-	torus3.rotation.x += ADD;
-	torus3.rotation.y += ADD;
 
 	renderer.render(scene, camera);
 	requestAnimationFrame(mainLoop);
